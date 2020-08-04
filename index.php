f91d4efbda78e9f726a30b47162491e72b2ae952
Alvaro Erazo <ader148@hotmail.com>
2020-08-04 12:59:31 -0500

    inicio de proyecto con cambios de interfaz y transicion

---
+++ index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+
+    <div class="row">
+
+        <div id="tail">
+            <img src="https://picsum.photos/200/200?grayscale">
+        </div>
+
+       <!-- <div class='something'> stuff </div>-->
+
+
+    </div>
+
+    <div class="row" style="z-index: 999; background-color: transparent;">
+        <div data-content-for="xl col-sm-12" style="padding-left: 6%; padding-top: 5%; padding-bottom: 15%;">
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/alvalle-2" data-mce-href="https://santiamen.com.es/project/alvalle-2"><span class="font_s5648sjjy" style="font-family: GraphikWeb-Regular, sans-serif; letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="font-family: GraphikWeb-Regular, sans-serif; letter-spacing: -0.0555556rem; color: #c6c6c6;">A tomato doesn't taste like a tomato.</span></a></p>
+            <p class="something"style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/koipe" data-mce-href="https://santiamen.com.es/project/koipe"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">All you have in the fridge is one egg.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/koipechatbot" data-mce-href="https://santiamen.com.es/project/koipechatbot"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">An artificial intelligence is hungry.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/carbonell" data-mce-href="https://santiamen.com.es/project/carbonell"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">A country use the same oil since 1854.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/rosas" data-mce-href="https://santiamen.com.es/project/rosas"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">You stay too long at the agency at Xmas.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/bertolli" data-mce-href="https://santiamen.com.es/project/bertolli"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">You get lost on YouTube recipes.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 4.5rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 4.5rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/alwidyan" data-mce-href="https://santiamen.com.es/project/alwidyan"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">Someone decides to build a city in the middle of the desert.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" href="https://santiamen.com.es/project/comite" data-mce-href="https://santiamen.com.es/project/comite"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">A design studio wants to flirt with their clients.</span></a></p>
+            <p class="something" style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;" data-mce-style="font-size: 2.72222rem; line-height: 2.83333rem; text-align: left;"><a clas="tagA" data-mce-href="https://santiamen.com.es/project/koipe_2019" href="https://santiamen.com.es/project/koipe_2019" data-mce-selected="1"><span class="font_s5648sjjy" style="letter-spacing: -0.0555556rem; color: #c6c6c6;" data-mce-style="letter-spacing: -0.0555556rem; color: #c6c6c6;">There are some strange sounds in the kitchen.</span></a></p>
+        </div>
+    </div>
+
+
+    <style>
+        .tagA {
+            text-decoration: none;
+        }
+
+        a:hover {
+            /*color: transparent;*/
+            text-decoration: none;
+        }
+
+        span.font_s5648sjjy:hover {
+            color: black !important;
+        }
+
+        /* estylos seguimiento */
+
+        #tail {
+            /* estulos iniciales*/
+            /*
+            position: absolute;
+            /* //////position: fixed; /////*/
+            /*display: none;
+            z-index: 1;*/
+
+
+            position: absolute;
+            /*display: none;*/
+            z-index: 1;
+
+            visibility: hidden;
+            opacity: 0;
+            
+        }
+
+        .something{
+            z-index: 999;
+            position: relative;
+        }
+        
+
+        /*.something {
+            width: 200px;
+            height: 160px;
+            background: yellow;
+        }*/
+    </style>
+
+
+    <script>
+        $('.something').bind('mousemove', function(e) {
+            $('#tail').css({
+                /*left: e.pageX  + 15,
+                top: e.pageY + 4*/
+                left: e.pageX - 80 ,
+                top: e.pageY - 80 
+            });
+        });
+
+        $('.something').bind('mouseleave', function(e) {
+            $('#tail').css({
+                //display: 'none',
+                visibility: 'hidden',
+                opacity: '0',
+                transition: ''
+            });
+        });
+
+        
+        $('.something').bind('mouseenter', function(e) {
+            $('#tail').css({
+                //display: 'block',
+                visibility: 'visible',
+                opacity: '1',
+                //transition: 'visibility 1s, opacity 0.5s linear'
+                transition: 'visibility 1s, opacity 0.5s ease-in-out'
+            });
+        });
+    </script>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
